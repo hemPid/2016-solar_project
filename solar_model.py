fdbeb7b5c640bcac0fa55ee1799b9b1c26d63631
hemPid <yrchikovivan@gmail.com>
2022-11-22 10:17:20 +0300

    move_space_object added

--- solar_model.py
+++ solar_model.py
@@ -32,10 +32,9 @@ def move_space_object(body, dt):
     **body** — тело, которое нужно переместить.
     """
 
-    ax = body.Fx/body.m
-    body.x += 42  # FIXME: не понимаю как менять...
-    body.Vx += ax*dt
-    # FIXME: not done recalculation of y coordinate!
+    a = body.force/body.m
+    body.vel += a*dt
+    body.pos += body.vel*dt
 
 
 def recalculate_space_objects_positions(space_objects, dt):
